330d22a58875dc3b6de49b36f5f240a5cbb44aad
zhangbin <hunter.zhangbin@gmail.com>
2015-02-21 23:27:07 +0800

    path修改

--- bootstrap/paths.php
+++ bootstrap/paths.php
@@ -1,5 +1,5 @@
 <?php
-define('SITE_ROOT', __DIR__);
+define('SITE_ROOT', __DIR__.'/../');
 define('SITE_URL',			'http://'.$_SERVER['SERVER_NAME'].'/ludo-php-2');
 define('LD_APP_PATH', SITE_ROOT.'/app');
 define('LD_CTRL_PATH', LD_APP_PATH.'/controllers');
