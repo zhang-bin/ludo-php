821796e01506b339b5946553becff53587727ffe
zhang bin <hunter.zhangbin@gmail.com>
2017-04-05 18:18:21 +0800

    ide helper脚本生成 Encrypter改成非static调用

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,43 +1,64 @@
 <?php
+namespace  {
+    exit("This file should not be included, only analyzed by your IDE");
+}
+
+namespace Ludo\Support\Facades {
+
+    class Crypt {
+        /**
+         * Encrypt the given value.
+         *
+         * @param $value
+         * @return string
+         */
+        public static function encrypt($value) {
+            return \Ludo\Encrypter\Encrypter::encrypt($value);
+        }
+
+        /**
+         * Decrypt the given value.
+         *
+         * @param $payload
+         * @return string
+         */
+        public static function decrypt($payload) {
+            return \Ludo\Encrypter\Encrypter::decrypt($payload);
+        }
+
+        /**
+         * Determine if the given key and cipher combination is valid.
+         *
+         * @param $key
+         * @param $cipher
+         * @return bool
+         */
+        public static function supported($key, $cipher) {
+            return \Ludo\Encrypter\Encrypter::supported($key, $cipher);
+        }
+
+    }
+}
+
 namespace {
-    exit('Only to be used as an helper for your IDE');
-    class Filter extends \Ludo\Support\Filter {
+    class Crypt extends \Ludo\Support\Facades\Crypt {}
 
-    }
+    class Filter extends \Ludo\Support\Filter {}
 
-    class Validator extends \Ludo\Support\Validator {
+    class Validator extends \Ludo\Support\Validator {}
 
-    }
+    class Lang extends \Ludo\Foundation\Lang {}
 
-    class Lang extends \Ludo\Foundation\Lang {
+    class Config extends \Ludo\Config\Config {}
 
-    }
+    class Factory extends \Ludo\Support\Factory {}
 
-    class Config extends \Ludo\Config\Config {
+    class View extends \Ludo\View\View {}
 
-    }
+    class TaskQueue extends \Ludo\Task\TaskQueue {}
 
-    class Factory extends \Ludo\Support\Factory {
+    class QueryException extends \Ludo\Database\QueryException {}
 
-    }
+    class Counter extends \Ludo\Counter\Counter {}
 
-    class View extends \Ludo\View\View {
-
-    }
-
-    class TaskQueue extends \Ludo\Task\TaskQueue {
-
-    }
-
-    class QueryException extends \Ludo\Database\QueryException {
-
-    }
-
-    class Counter extends Ludo\Counter\Counter {
-
-    }
-
-    class Encrypter extends Ludo\Encrypter\Encrypter {
-
-    }
 }
--- app/controllers/Tools.php
+++ app/controllers/Tools.php
@@ -77,4 +77,97 @@ class Tools extends BaseCtrl {
         }
         redirect('tools/lang');
     }
+
+    public function ideHelperGenerate() {
+        $serviceProviders = [
+            \Ludo\Support\Facades\Crypt::class
+        ];
+        $helper = '<pre>';
+        $newLine = "\n";
+        $blank = '    ';
+        $helper .= 'namespace  {
+    exit("This file should not be included, only analyzed by your IDE");
+}
+';
+        $helper .= $newLine;
+        $helper .= 'namespace Ludo\Support\Facades {';
+        $helper .= $newLine;
+        foreach ($serviceProviders as $serviceProvider) {
+            $facadeAccessor = $serviceProvider::getFacadeAccessor();
+            $obj = \Ludo\Support\ServiceProvider::getInstance()->getRegisteredAbstract($facadeAccessor);
+            $className = get_class($obj);
+            $reflection = new ReflectionClass($className);
+            $helper .= $newLine;
+            $helper .= $blank.'class '.array_pop(explode('\\', $serviceProvider)).' {';
+            $helper .= $newLine;
+            $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+            foreach ($methods as $method) {
+                if ($method->getName() == '__construct') continue;
+                $comment = $blank.str_replace("\n", "\n".$blank, $method->getDocComment());
+                $helper .= $blank.$comment;
+                $helper .= $newLine;
+                $helper .= $blank.$blank.'public static function '.$method->getName().'(';
+                $parameters = $method->getParameters();
+                $param = array();
+                foreach ($parameters as $parameter) {
+                    $paramStr = '$'.$parameter->getName();
+                    if ($parameter->isOptional()) {
+                        $default = $parameter->isDefaultValueAvailable() ? $parameter->getDefaultValue() : null;
+                        if (is_bool($default)) {
+                            $default = $default ? 'true' : 'false';
+                        } elseif (is_array($default)) {
+                            $default = 'array()';
+                        } elseif (is_null($default)) {
+                            $default = 'null';
+                        } else {
+                            $default = "'" . trim($default) . "'";
+                        }
+                        $paramStr .= " = $default";
+                    }
+                    $param[] = $paramStr;
+                }
+                $helper .= implode(', ', $param).') {';
+                $helper .= $newLine;
+
+                $helper .= $blank.$blank.$blank;
+                if (!stristr($comment, '@return void')) {
+                    $helper .= 'return ';
+                }
+                $helper .= '\\'.$className.'::'.$method->getName().'('.implode(', ', $param).');';
+                $helper .= $newLine;
+                $helper .= $blank.$blank.'}';
+                $helper .= $newLine;
+                $helper .= $newLine;
+            }
+            $helper .= $blank.'}';
+        }
+        $helper .= $newLine;
+        $helper .= '}';
+
+
+        $serviceProviders = [
+            \Ludo\Support\Facades\Crypt::class,
+            \Ludo\Support\Filter::class,
+            \Ludo\Support\Validator::class,
+            \Ludo\Foundation\Lang::class,
+            \Ludo\Config\Config::class,
+            \Ludo\Support\Factory::class,
+            \Ludo\View\View::class,
+            \Ludo\Task\TaskQueue::class,
+            \Ludo\Database\QueryException::class,
+            \Ludo\Counter\Counter::class
+        ];
+
+        $helper .= $newLine.$newLine;
+        $helper .= 'namespace {';
+        $helper .= $newLine;
+        foreach ($serviceProviders as $serviceProvider) {
+            $helper .= $blank.'class '.array_pop(explode('\\', $serviceProvider)).' extends \\'.$serviceProvider.' {}';
+            $helper .= $newLine;
+            $helper .= $newLine;
+        }
+        $helper .= '}';
+        $helper .= '</pre>';
+        echo $helper;
+    }
 }
