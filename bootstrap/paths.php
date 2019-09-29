5bd17b70204ef78802767268fb6d3efeac922813
zhang bin <hunter.zhangbin@gmail.com>
2019-09-29 14:14:29 +0800

    代码重构

--- bootstrap/paths.php
+++ bootstrap/paths.php
@@ -8,7 +8,7 @@ define('LD_HELPER_PATH',	LD_APP_PATH.'/Helpers');
 define('LD_CONF_PATH',		LD_APP_PATH.'/Conf');
 define('TPL_ROOT', 			LD_APP_PATH.'/Templates');
 define('LD_PORTAL_URL',		SITE_URL.'/index.php');
-define('LD_PUBLIC_PATH',	SITE_URL.'/public');
+define('LD_PUBLIC_URL',	SITE_URL.'/public');
 define('LD_UPLOAD_PATH',	SITE_ROOT.'/uploads');
 define('LD_UPLOAD_URL',		SITE_URL.'/uploads');
 define('LD_UPLOAD_IMG_PATH',LD_UPLOAD_PATH.'/img');
