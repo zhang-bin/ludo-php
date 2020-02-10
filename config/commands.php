135c0072da368247089a46733474afb53e7b18ca
zhang bin <hunter.zhangbin@gmail.com>
2020-02-10 11:34:32 +0800

    config

--- config/commands.php
+++ config/commands.php
@@ -3,4 +3,5 @@ return [
     Ludo\AsyncTask\Command\FlushMessageCommand::class,
     Ludo\AsyncTask\Command\ReloadMessageCommand::class,
     Ludo\Server\Command\ServerCommand::class,
+    Ludo\Process\Command\ProcessCommand::class,
 ];
